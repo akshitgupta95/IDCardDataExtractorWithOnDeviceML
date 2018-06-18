8b70e3cc2d49eee0c7a4d4c417f45ad403452439
B0201234 <b0201234@My-Macbook-Pro.local>
2018-06-18 17:46:12 +0530

    removed Credidentials

---
+++ app/src/main/java/com/gmail/akshitgupta1695/textextractor/MainActivity.java
@@ -0,0 +1,158 @@
+package com.gmail.akshitgupta1695.textextractor;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button takePicture;
+    private Button ocr;
+    private ImageView imageview;
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private Bitmap mImageBitmap;
+    private TextView mTextView;
+    private CardView mCardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePicture=(Button)findViewById(R.id.clickImage);
+        ocr=(Button)findViewById(R.id.ocr);
+        ocr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTxt();
+            }
+        });
+        imageview=(ImageView)findViewById(R.id.imageview);
+        mTextView=(TextView)findViewById(R.id.textView);
+        mCardView=(CardView)findViewById(R.id.container_card_view);
+
+    }
+
+    public void takePicture(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this,"Error creating file",Toast.LENGTH_SHORT).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+                mImageBitmap = getBitmap(mCurrentPhotoPath);
+                imageview.setImageBitmap(mImageBitmap);
+
+        }
+    }
+
+    public Bitmap getBitmap(String path) {
+        try {
+
+            File f= new File(path);
+             Bitmap myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+           return myBitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }}
+
+    private void detectTxt() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mImageBitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                processTxt(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    private void processTxt(FirebaseVisionText text) {
+        List<FirebaseVisionText.Block> blocks = text.getBlocks();
+        if (blocks.size() == 0) {
+            Toast.makeText(MainActivity.this, "No Text :(", Toast.LENGTH_LONG).show();
+            return;
+        }
+        StringBuilder textBuilder=new StringBuilder();
+        for (FirebaseVisionText.Block block : text.getBlocks()) {
+            String txt = block.getText();
+            textBuilder.append(txt+"\n");
+
+
+        }
+        mCardView.setVisibility(View.VISIBLE);
+        mTextView.setText(textBuilder.toString());
+    }
+}
